b2edd2f76832e951ce869a32cd6f5fd8b89857bc
alehuo <aleksi.huotala@helsinki.fi>
2016-09-15 22:07:20 +0300

    Change server port to 80 for easier access

--- src/main/java/tikape/runko/Main.java
+++ src/main/java/tikape/runko/Main.java
@@ -10,11 +10,16 @@ import tikape.runko.database.OpiskelijaDao;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        Database database = new Database("jdbc:sqlite:opiskelijat.db");
+        //Tietokannan alustus
+        Database database = new Database("jdbc:sqlite:keskustelualue.db");
         database.init();
 
-        OpiskelijaDao opiskelijaDao = new OpiskelijaDao(database);
-
+//        OpiskelijaDao opiskelijaDao = new OpiskelijaDao(database);
+        
+        //Portti 80 jotta päästään selaamaan projektia suoraan osoitteessa http://localhost (Muistetaan silti poistaa tämä kun projekti viedään Herokuun.)
+        port(80);
+       
+        //Etusivu
         get("/", (req, res) -> {
             HashMap map = new HashMap<>();
             map.put("viesti", "tervehdys");
@@ -22,18 +27,18 @@ public class Main {
             return new ModelAndView(map, "index");
         }, new ThymeleafTemplateEngine());
 
-        get("/opiskelijat", (req, res) -> {
-            HashMap map = new HashMap<>();
-            map.put("opiskelijat", opiskelijaDao.findAll());
-
-            return new ModelAndView(map, "opiskelijat");
-        }, new ThymeleafTemplateEngine());
-
-        get("/opiskelijat/:id", (req, res) -> {
-            HashMap map = new HashMap<>();
-            map.put("opiskelija", opiskelijaDao.findOne(Integer.parseInt(req.params("id"))));
-
-            return new ModelAndView(map, "opiskelija");
-        }, new ThymeleafTemplateEngine());
+//        get("/opiskelijat", (req, res) -> {
+//            HashMap map = new HashMap<>();
+//            map.put("opiskelijat", opiskelijaDao.findAll());
+//
+//            return new ModelAndView(map, "opiskelijat");
+//        }, new ThymeleafTemplateEngine());
+//
+//        get("/opiskelijat/:id", (req, res) -> {
+//            HashMap map = new HashMap<>();
+//            map.put("opiskelija", opiskelijaDao.findOne(Integer.parseInt(req.params("id"))));
+//
+//            return new ModelAndView(map, "opiskelija");
+//        }, new ThymeleafTemplateEngine());
     }
 }
