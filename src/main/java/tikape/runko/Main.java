5c7a762be3fc8fdbe40d6c2edeebeeff1ab0da41
alehuo <aleksi.huotala@helsinki.fi>
2016-09-18 18:09:10 +0300

    Add few routes for browsing and posting new threads

--- src/main/java/tikape/runko/Main.java
+++ src/main/java/tikape/runko/Main.java
@@ -17,38 +17,38 @@ public class Main {
         get("/", (req, res) -> {
             HashMap map = new HashMap<>();
             map.put("viesti", "tervehdys");
-
+            //Tähän näkymä, jolla näytetään etusivu (kategoriat ja alakategoriat)
             return new ModelAndView(map, "index");
         }, new ThymeleafTemplateEngine());
         //Näytä viestiketju
         get("/thread/:threadId", (req, res) -> {
             int id = Integer.parseInt(req.params("threadId"));
-
+            //Tähän näkymä, jossa näytetään viestiketju
             return "Viestiketjun id: " + id;
         });
         //Lähetä viestiketjuun uusi vastaus
         post("/thread/:threadId", (req, res) -> {
             int id = Integer.parseInt(req.params("threadId"));
-
+            //Käsitellään tässä POST-pyynnön data ja lisätään tietokantaan
             return "Vastaus viestiketjuun, jolla id: " + id;
         });
         //Näytä alakategorian viestit:
         get("/subcategory/:subCategoryId", (req, res) -> {
             int id = Integer.parseInt(req.params("subCategoryId"));
-
+            //Tähän näkymä, jossa näytetään alakategorian viestit
             return "Alakategorian id: " + id;
+        });
+        //Uuden viestiketjun lähettäminen:
+        post("/subcategory/:subCategoryId", (req, res) -> {
+            int id = Integer.parseInt(req.params("subCategoryId"));
+            //Käsitellään tässä POST-pyynnön data ja lisätään tietokantaan
+            return "Tällä käsitellään viestiketjun data alakategoriaan " + id + ".";
         });
         //Uuden viestiketjun luominen:
         get("/new/:subCategoryId", (req, res) -> {
             int id = Integer.parseInt(req.params("subCategoryId"));
-
+            //Näytetään tässä lomake käyttäjälle
             return "Tällä luodaan uusi viestiketju alakategoriaan " + id + ".";
         });
-        //Uuden viestiketjun lähettäminen:
-        post("/new/:subCategoryId", (req, res) -> {
-            int id = Integer.parseInt(req.params("subCategoryId"));
-            return "Tällä käsitellään viestiketjun data alakategoriaan " + id + ".";
-        });
-
     }
 }
