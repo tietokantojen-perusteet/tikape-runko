364eeea2bfda9099351626eb1a14bfcf7d9f5b15
alehuo <aleksi.huotala@helsinki.fi>
2016-09-24 21:56:18 +0300

    Add Thread DAO & class (WIP)

---
+++ src/main/java/tikape/runko/database/ThreadDao.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tikape.runko.database;
+
+import java.sql.SQLException;
+import java.util.List;
+import tikape.runko.domain.Thread;
+
+/**
+ *
+ * @author Aleksi Huotala
+ */
+public class ThreadDao implements Dao<Thread, Integer> {
+
+    @Override
+    public Thread findOne(Integer key) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Thread> findAll() throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete(Integer key) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
