d673a508c531a2005c8c620af0fbe6e535620b92
Tamir Ella M <tamella@da5-cs-b221-19.helsinki.fi>
2016-09-27 18:06:59 +0300

    korjattu daot

---
+++ src/main/java/tikape/runko/database/KeskusteluDao.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tikape.runko.database;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import tikape.runko.domain.Alue;
+import tikape.runko.domain.Keskustelu;
+import java.util.Date;
+
+/**
+ *
+ * @author tamella
+ */
+public class KeskusteluDao implements Dao<Keskustelu, Integer>{
+    private Database database;
+    private Dao<Alue, Integer> aluedao;
+    
+    public KeskusteluDao(Database base, Dao<Alue, Integer> aluedao ){
+        this.database = base;
+        this.aluedao = aluedao;
+    }
+    @Override
+    public Keskustelu findOne(Integer key) throws SQLException {
+        Connection connection = database.getConnection();
+        PreparedStatement stmt = connection.prepareStatement("SELECT * FROM Keskustelu WHERE keskustelu_id = ?");
+        stmt.setObject(1, key);
+
+        ResultSet rs = stmt.executeQuery();
+        boolean hasOne = rs.next();
+        if (!hasOne) {
+            return null;
+        }
+
+        Integer keskusteluid = rs.getInt("keskustelu_id");
+        String otsikko = rs.getString("otsikko");
+        String aloittaja = rs.getString("aloittaja");
+        String aloitusviesti = rs.getString("aloitusviesti");
+        Date paivamaara = rs.getDate("paivamaara");
+        
+
+        Keskustelu k = new Keskustelu(keskusteluid, otsikko, aloittaja, aloitusviesti, paivamaara);
+        
+        Integer omaalue = rs.getInt("omaalue");
+        
+        k.setOmaalue(this.aluedao.findOne(omaalue));
+        
+        rs.close();
+        stmt.close();
+        connection.close();
+
+        return k;
+    }
+
+    @Override
+    public List<Keskustelu> findAll() throws SQLException {
+
+        Connection connection = database.getConnection();
+        PreparedStatement stmt = connection.prepareStatement("SELECT * FROM Keskustelu");
+
+        ResultSet rs = stmt.executeQuery();
+        List<Keskustelu> keskustelut = new ArrayList<>();
+        while (rs.next()) {
+            Integer keskusteluid = rs.getInt("keskustelu_id");
+            String otsikko = rs.getString("otsikko");
+            String aloittaja = rs.getString("aloittaja");
+            String aloitusviesti = rs.getString("aloitusviesti");
+            Integer omaa = rs.getInt("omaalue");
+            Date paivamaara = rs.getDate("paivamaara");
+
+            Keskustelu palapala = new Keskustelu(keskusteluid, otsikko, aloittaja, aloitusviesti, paivamaara);
+            palapala.setOmaalue(this.aluedao.findOne(omaa));
+            keskustelut.add(palapala);
+        }
+
+        rs.close();
+        stmt.close();
+        connection.close();
+
+        return keskustelut;
+    }
+
+    @Override
+    public void delete(Integer key) throws SQLException {
+        // ei toteutettu
+    }
+}
