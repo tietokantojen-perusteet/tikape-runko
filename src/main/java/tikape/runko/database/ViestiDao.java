ef9218381c4cccb16eafba50298f6afab7e45f6c
Tamir Ella M <tamella@da3-cs-dk110-05.helsinki.fi>
2016-09-30 17:27:28 +0300

    viestidaoon lisätty joitain kyselyitä

--- src/main/java/tikape/runko/database/ViestiDao.java
+++ src/main/java/tikape/runko/database/ViestiDao.java
@@ -5,10 +5,95 @@
  */
 package tikape.runko.database;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import tikape.runko.domain.Alue;
+import tikape.runko.domain.Keskustelu;
+import tikape.runko.domain.Viesti;
+
 /**
  *
  * @author tamella
  */
-public class ViestiDao {
+// tämä luokka koostuu niistä kyselyistä, jotka taululle Viesti voidaan esittää
+public class ViestiDao implements Dao<Viesti, Integer>{
+    private Database database;
+    private Dao<Keskustelu, Integer> keskusdao;
+    
+    public ViestiDao(Database base, Dao<Keskustelu, Integer> keskusdao ){
+        this.database = base;
+        this.keskusdao = keskusdao;
+    }
+    @Override
+    public Viesti findOne(Integer key) throws SQLException {
+        Connection connection = database.getConnection();
+        PreparedStatement stmt = connection.prepareStatement("SELECT * FROM Viesti WHERE viesti_id = ?");
+        stmt.setObject(1, key);
+
+        ResultSet rs = stmt.executeQuery();
+        boolean hasOne = rs.next();
+        if (!hasOne) {
+            return null;
+        }
+
+        Integer id = rs.getInt("viesti_id");
+        Date paivamaara = rs.getDate("julkaisuaika");
+        String kirjoittaja = rs.getString("kirjoittaja");
+        String teksti = rs.getString("teksti");
+        
+        
+
+        Viesti v = new Viesti(id, paivamaara, kirjoittaja, teksti);
+        
+        Integer omakeskustelu = rs.getInt("omakeskustelu");
+        
+        v.setOmakeskustelu(this.keskusdao.findOne(omakeskustelu));
+        
+        rs.close();
+        stmt.close();
+        connection.close();
+
+        return v;
+    }
+
+    @Override
+    public List<Viesti> findAll() throws SQLException {
+
+        Connection connection = database.getConnection();
+        PreparedStatement stmt = connection.prepareStatement("SELECT * FROM Viesti");
+
+        ResultSet rs = stmt.executeQuery();
+        List<Viesti> viestit = new ArrayList<>();
+        while (rs.next()) {
+            Integer id = rs.getInt("viesti_id");
+            Date paivamaara = rs.getDate("julkaisuaika");
+            String kirjoittaja = rs.getString("kirjoittaja");
+            String teksti = rs.getString("teksti");
+            
+            Integer omakes = rs.getInt("omakeskustelu");
+            
+            
+
+            Viesti tekstiteksti = new Viesti(id, paivamaara, kirjoittaja, teksti);
+            tekstiteksti.setOmakeskustelu(this.keskusdao.findOne(omakes));
+            viestit.add(tekstiteksti);
+        }
+
+        rs.close();
+        stmt.close();
+        connection.close();
+
+        return viestit;
+    }
+
+    @Override
+    public void delete(Integer key) throws SQLException {
+        // ei toteutettu
+    }
     
 }
