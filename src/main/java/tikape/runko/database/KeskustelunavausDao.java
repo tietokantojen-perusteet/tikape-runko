1775492cb51ae9e6c6f4afd28865a9753e463f00
Janne <suom.jann@gmail.com>
2016-10-06 14:04:21 +0300

    KeskustelunavausDaon hahmotelmaa

---
+++ src/main/java/tikape/runko/database/KeskustelunavausDao.java
@@ -0,0 +1,26 @@
+package tikape.runko.database;
+
+import java.sql.SQLException;
+import java.util.List;
+import tikape.runko.domain.Keskustelunavaus;
+
+public class KeskustelunavausDao implements Dao<Keskustelunavaus, Integer> {
+
+    @Override
+    public Keskustelunavaus findOne(Integer key) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Keskustelunavaus> findAll() throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete(Integer key) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+    
+}
