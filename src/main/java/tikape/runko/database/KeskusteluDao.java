0e33296a7536d4bb78ebbfbd2a6bdea118abcb59
HyTom <HyyrylainenTommi@gmail.com>
2016-10-10 16:39:04 +0300

    Fisr cummit

---
+++ src/main/java/tikape/runko/database/KeskusteluDao.java
@@ -0,0 +1,24 @@
+package tikape.runko.database;
+
+import java.sql.SQLException;
+import java.util.List;
+
+
+public class KeskusteluDao implements Dao{
+
+    @Override
+    public Object findOne(Object key) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List findAll() throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List findRange(int first, int count) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
