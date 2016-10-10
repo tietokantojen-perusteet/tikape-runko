a77b9dd2f6bc834ad6645c223d9b98f802c0b57d
HyTom <HyyrylainenTommi@gmail.com>
2016-10-10 16:39:04 +0300

    Fisr cummit

---
+++ src/main/java/tikape/runko/database/AlueDao.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tikape.runko.database;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import tikape.runko.domain.Alue;
+
+public class AlueDao implements Dao<Alue, Integer> {
+
+    private Database database;
+
+    public AlueDao(Database database) {
+        this.database = database;
+    }
+
+    @Override
+    public Alue findOne(Integer key) throws SQLException {
+        Connection connection = database.getConnection();
+        PreparedStatement stmt = connection.prepareStatement("SELECT * FROM Alue WHERE id = ?");
+        stmt.setObject(1, key);
+
+        ResultSet rs = stmt.executeQuery();
+        boolean hasOne = rs.next();
+        if (!hasOne) {
+            return null;
+        }
+
+        Integer id = rs.getInt("id");
+        String nimi = rs.getString("nimi");
+
+        Alue alue = new Alue(id, nimi);
+
+        rs.close();
+        stmt.close();
+        connection.close();
+
+        return alue;
+    }
+
+    @Override
+    public List<Alue> findAll() throws SQLException {
+
+        Connection connection = database.getConnection();
+        PreparedStatement stmt = connection.prepareStatement("SELECT * FROM Alue");
+
+        ResultSet rs = stmt.executeQuery();
+        List<Alue> alueet = new ArrayList<>();
+        while (rs.next()) {
+            Integer id = rs.getInt("id");
+            String nimi = rs.getString("nimi");
+
+            alueet.add(new Alue(id, nimi));
+        }
+
+        rs.close();
+        stmt.close();
+        connection.close();
+
+        return alueet;
+    }
+
+    public void delete(Integer key) throws SQLException {
+        // ei toteutettu
+    }
+
+    @Override
+    public List<Alue> findRange(int first, int count) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
