d0c1dea15b170c33c16606233a22e2350aa677d2
jippo444 <toni.salama@gmail.com>
2016-10-12 20:54:57 +0300

    Lisätty muokatut html-tiedostot
    Mainiin lisätty myös staattinen tiedostosijainti sekä ensimmäinen get
    metodi juurelle.

--- src/main/java/tikape/runko/Main.java
+++ src/main/java/tikape/runko/Main.java
@@ -1,11 +1,16 @@
 package tikape.runko;
 
 import java.util.HashMap;
+import java.util.List;
 import spark.ModelAndView;
+import spark.Spark;
 import static spark.Spark.*;
 import spark.template.thymeleaf.ThymeleafTemplateEngine;
 import tikape.runko.database.Database;
 import tikape.runko.database.AlueDao;
+import tikape.runko.database.KeskusteluDao;
+import tikape.runko.database.ViestiDao;
+import tikape.runko.domain.Alue;
 
 public class Main {
 
@@ -15,28 +20,32 @@ public class Main {
         }
         Database database = new Database("jdbc:sqlite:keskustelualue.db");
         database.init();
+        
+        staticFileLocation("/public");
 
         AlueDao alueDao = new AlueDao(database);
+        KeskusteluDao keskusteluDao = new KeskusteluDao(database, alueDao);
+        ViestiDao viestiDao = new ViestiDao(database, keskusteluDao);
 
-        System.out.println("ASD");
 
-        get("/", (req, res) -> {
-            HashMap map = new HashMap<>();
-            //map.put("viesti", "tervehdys");
 
-            System.out.println("asd");
+        Spark.get ("/", (req, res) -> {
+            List<Alue> list = alueDao.findAll();
+            HashMap map = new HashMap();
+            map.put("alueet", list);
 
             return new ModelAndView(map, "index");
+
         }, new ThymeleafTemplateEngine());
 
-        get("/alueet", (req, res) -> {
+        Spark.get("/alueet", (req, res) -> {
             HashMap map = new HashMap<>();
             map.put("alueet", alueDao.findAll());
 
             return new ModelAndView(map, "alueet");
         }, new ThymeleafTemplateEngine());
 
-        get("/alueet/:id", (req, res) -> {
+        Spark.get("/alueet/:id", (req, res) -> {
             HashMap map = new HashMap<>();
             map.put("alue", alueDao.findOne(Integer.parseInt(req.params("id"))));
 
