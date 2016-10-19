6c85780c3e5fcb3a4a0cb7d434702d95d05a5d2d
mkmoisio <mmoisio@kiisseli.com>
2016-10-19 15:29:55 +0300

    Update Main.java
    lisätty get(":/", .. käsittelemään eri alueiden aiheiden listaus

--- src/main/java/tikape/runko/Main.java
+++ src/main/java/tikape/runko/Main.java
@@ -12,18 +12,28 @@ import tikape.runko.domain.*;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        // Käytetään testidataa
+             // Käytetään testidataa
         Database database = new Database("jdbc:sqlite:yksisarvistentestitietokanta.db");
         database.init();
 
         AlueDao alueDao = new AlueDao(database);
-
+        AiheDao aiheDao = new AiheDao(database, alueDao);
+        
         get("/", (req, res) -> {
             HashMap map = new HashMap<>();
             map.put("alueet", alueDao.findAll());
 
             return new ModelAndView(map, "index");
         }, new ThymeleafTemplateEngine());
+        
+        get("/:id", (req, res) -> {
+            
+            HashMap<String, Object> data = new HashMap<>();
+            data.put("aiheet", aiheDao.findAllInAlue(alueDao.findOne(Integer.parseInt(req.params(":id")))));
+            
+            return new ModelAndView(data, "area");
+        }, new ThymeleafTemplateEngine());
+        
 
 //        get("/opiskelijat/:id", (req, res) -> {
 //            HashMap map = new HashMap<>();
