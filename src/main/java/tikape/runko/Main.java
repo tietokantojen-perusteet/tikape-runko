f0297cffca34ff68ef1748998a249f8c5e8d703e
impliedfeline <kinnunen.jussi@hotmail.com>
2017-02-25 15:17:40 +0200

    init database class

--- src/main/java/tikape/runko/Main.java
+++ src/main/java/tikape/runko/Main.java
@@ -10,7 +10,7 @@ import tikape.runko.database.OpiskelijaDao;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        Database database = new Database("jdbc:sqlite:opiskelijat.db");
+        Database database = new Database("jdbc:sqlite:tietokanta.db");
         database.init();
 
         OpiskelijaDao opiskelijaDao = new OpiskelijaDao(database);
--- src/main/java/tikape/runko/database/Database.java
+++ src/main/java/tikape/runko/database/Database.java
@@ -39,10 +39,35 @@ public class Database {
         ArrayList<String> lista = new ArrayList<>();
 
         // tietokantataulujen luomiseen tarvittavat komennot suoritusjärjestyksessä
-        lista.add("CREATE TABLE Opiskelija (id integer PRIMARY KEY, nimi varchar(255));");
-        lista.add("INSERT INTO Opiskelija (nimi) VALUES ('Platon');");
-        lista.add("INSERT INTO Opiskelija (nimi) VALUES ('Aristoteles');");
-        lista.add("INSERT INTO Opiskelija (nimi) VALUES ('Homeros');");
+        lista.add("CREATE TABLE Lauta\n"
+                + "(\n"
+                + "nimi varchar(20) PRIMARY KEY,\n"
+                + "motd varchar(50)\n"
+                + ");");
+        lista.add("CREATE TABLE Lanka\n"
+                + "(\n"
+                + "id integer PRIMARY KEY,\n"
+                + "otsikko varchar(50),\n"
+                + "lauta varchar(20) NOT NULL,\n"
+                + "FOREIGN KEY(lauta) REFERENCES Lauta(nimi)\n"
+                + ")");
+        lista.add("CREATE TABLE Viesti\n"
+                + "(\n"
+                + "id integer PRIMARY KEY,\n"
+                + "sisalto varchar(1000) NOT NULL,\n"
+                + "nimimerkki varchar(20),\n"
+                + "aika timestamp,\n"
+                + "lanka_id integer NOT NULL,\n"
+                + "vastaus_id integer,\n"
+                + "FOREIGN KEY(lanka_id) REFERENCES Lanka(id),\n"
+                + "FOREIGN KEY(vastaus_id) REFERENCES Viesti(id)\n"
+                + ");")
+                
+//        Model for db tables
+//        lista.add("CREATE TABLE Opiskelija (id integer PRIMARY KEY, nimi varchar(255));");
+//        lista.add("INSERT INTO Opiskelija (nimi) VALUES ('Platon');");
+//        lista.add("INSERT INTO Opiskelija (nimi) VALUES ('Aristoteles');");
+//        lista.add("INSERT INTO Opiskelija (nimi) VALUES ('Homeros');");
 
         return lista;
     }
