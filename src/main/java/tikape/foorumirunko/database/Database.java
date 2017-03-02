1d77dbe80c0d690bb003e5452cc97a54e47015c2
Joonas Saha <joonassaha@Joonass-MacBook-Pro.local>
2017-03-02 22:49:44 +0200

    database-luokka tehty. Dao:sta ei vielä hajua

--- src/main/java/tikape/foorumirunko/database/Database.java
+++ src/main/java/tikape/foorumirunko/database/Database.java
@@ -1,17 +1,49 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tikape.foorumirunko.database;
 
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
-/**
- *
- * @author eemitant
- */
 public class Database {
+
+    private String databaseAddress;
+
+    public Database(String databaseAdd) throws ClassNotFoundException {
+        this.databaseAddress = databaseAdd;
+    }
+
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(databaseAddress);
+    }
+
+    public void init() {
+        List<String> lauseet = createTableLauseet();
+
+        // "try with resources" sulkee resurssin automaattisesti lopuksi
+        try (Connection conn = getConnection()) {
+            Statement st = conn.createStatement();
+
+            // suoritetaan komennot
+            for (String lause : lauseet) {
+                System.out.println("Running command >> " + lause);
+                st.executeUpdate(lause);
+            }
+
+        } catch (Throwable t) {
+            // jos tietokantataulu on jo olemassa, ei komentoja suoriteta
+            System.out.println("Error >> " + t.getMessage());
+        }
+    }
+
+    private List<String> createTableLauseet() {
+        ArrayList<String> lista = new ArrayList<>();
+        
+        // tietokantataulujen luomiseen tarvittavat komennot suoritusjärjestyksessä
+        lista.add("CREATE TABLE Kayttaja (kayttajan_id integer PRIMARY KEY, nimimerkki varchar(50));");
+        lista.add("CREATE TABLE Alue (alueen_id integer PRIMARY KEY, alueen_nimi varchar(200), viestien_maara integer, viimeisinViesti timestamp;");
+        lista.add("CREATE TABLE Viesti (kayttaja integer, alue integer, otsikko varchar(200), sisalto varchar(3000), aika timestamp, FOREIGN KEY(kayttaja) REFERENCES Kayttaja(kayttajan_id), FOREIGN KEY(alue) REFERENCES Alue(alueen_id);");
+        
+        return lista;
+    }
 
 }
