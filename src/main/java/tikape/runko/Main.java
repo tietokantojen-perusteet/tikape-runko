bf1f1bcd24078c507bf9e2103418e8f9a38abdd6
Tomeitous <lolled.mr@gmail.com>
2017-03-04 14:16:47 +0200

    polku aiheesta ketjuun lisätty

--- src/main/java/tikape/runko/Main.java
+++ src/main/java/tikape/runko/Main.java
@@ -8,7 +8,10 @@ import static spark.Spark.*;
 import spark.template.thymeleaf.ThymeleafTemplateEngine;
 import tikape.runko.database.AihealueDao;
 import tikape.runko.database.Database;
+import tikape.runko.database.KetjuDao;
+import tikape.runko.database.ViestiDao;
 import tikape.runko.domain.Aihealue;
+import tikape.runko.domain.Ketju;
 
 public class Main {
 
@@ -17,16 +20,15 @@ public class Main {
         database.init();
 
         AihealueDao aDao = new AihealueDao(database);
+        KetjuDao kDao = new KetjuDao(database);
+        ViestiDao vDao = new ViestiDao(database);
 
- 
-
-
-      get("/", (req, res) -> {
+        get("/", (req, res) -> {
             HashMap map = new HashMap<>();
             map.put("aihealueet", aDao.findAll());
             return new ModelAndView(map, "index");
         }, new ThymeleafTemplateEngine());
-        
+
         post("/", (req, res) -> {
             String aihe = req.queryParams("aihe");
             System.out.println(aihe);
@@ -34,18 +36,19 @@ public class Main {
                 res.redirect("/");
                 return null;
             }
-            
+
             aDao.add(new Aihealue(aihe));
             res.redirect("/");
-            
+
             return null;
         });
 
-        
+        get("Aihealue/:aihealue_id", (request, response) -> {
 
-
-        
- 
+            HashMap map = new HashMap<>();
+            map.put("ketjut", kDao.findAll());
+            return new ModelAndView(map, "ketjut");
+        }, new ThymeleafTemplateEngine());
 
     }
 }
