69cd5c34c80e37e014f89bd84ec8e32260535e9f
jyrkilouhi <jyrkilouhi@gmail.com>
2017-06-05 14:17:12 +0300

    bit more

--- src/main/java/tikape/runko/Main.java
+++ src/main/java/tikape/runko/Main.java
@@ -8,6 +8,7 @@ import tikape.runko.database.AiheDao;
 import tikape.runko.database.AlueDao;
 import tikape.runko.database.Database;
 import tikape.runko.database.OpiskelijaDao;
+import tikape.runko.domain.Aihe;
 
 public class Main {
 
@@ -27,11 +28,24 @@ public class Main {
 
         get("/alueet/:id", (req, res) -> {
             HashMap map = new HashMap<>();
-            System.out.println("Alue: " + req.params("id"));
             map.put("alue", alueDao.findOne(Integer.parseInt(req.params("id"))));
             map.put("aiheet", aiheDao.alueenAiheet(Integer.parseInt(req.params("id"))));
 
             return new ModelAndView(map, "aiheet");
+        }, new ThymeleafTemplateEngine());
+        
+        get("/aiheet/:id", (req, res) -> {
+            HashMap map = new HashMap<>();
+            System.out.println("Aihe: " + req.params("id"));
+            Aihe aihe = aiheDao.findOne(Integer.parseInt(req.params("id")));
+            map.put("alue", alueDao.findOne(aihe.getAlue_id()));
+            map.put("aihe", aihe);
+            map.put("viestit", null);
+            // tämä kesken eli viestiDao tekemättä
+            //map.put("viestit", viestiDao.aiheenViestit());
+            
+
+            return new ModelAndView(map, "viestit");
         }, new ThymeleafTemplateEngine());
     }
 }
