06b6700a42d8a093090560b07d931b248e34ffe2
jyrkilouhi <jyrkilouhi@gmail.com>
2017-06-06 09:16:19 +0300

    06.06.2017

--- src/main/java/tikape/runko/database/ViestiDao.java
+++ src/main/java/tikape/runko/database/ViestiDao.java
@@ -17,6 +17,35 @@ public class ViestiDao implements Dao<Viesti, Integer>{
     public ViestiDao(Database database) {
         this.database = database;
     }
+    
+        public Viesti create(Viesti uusiViesti) throws SQLException {
+        Connection connection = database.getConnection();
+        PreparedStatement stmt = connection.prepareStatement("INSERT INTO Viesti (aihe_id, teksti, nimimerkki) "
+                + "VALUES ( ? , ? , ? )");
+        stmt.setObject(1, uusiViesti.getAihe_id());
+        stmt.setObject(2, uusiViesti.getTeksti());
+        stmt.setObject(3, uusiViesti.getNimimerkki());        
+        stmt.execute();
+        
+        stmt = connection.prepareStatement("SELECT viesti_id, ajankohta FROM Viesti "
+                + "WHERE aihe_id = ? AND nimimerkki = ? "
+                + "ORDER BY ajankohta DESC;");       
+        stmt.setObject(1, uusiViesti.getAihe_id());
+        stmt.setObject(2, uusiViesti.getNimimerkki());
+        ResultSet rs = stmt.executeQuery();
+        
+        boolean hasOne = rs.next();
+        if (!hasOne) {
+            return null;
+        }
+        
+        int id = rs.getInt("viesti_id");
+        String ajankohta = rs.getString("ajankohta");
+        Viesti luotuViesti = new Viesti(id, uusiViesti.getAihe_id(), uusiViesti.getTeksti(), ajankohta , uusiViesti.getNimimerkki());
+        stmt.close();
+        connection.close();        
+        return luotuViesti;
+    }
 
     @Override
     public Viesti findOne(Integer key) throws SQLException {
