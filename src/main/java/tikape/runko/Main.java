c890f80d7ff23213cba9123a173d405d7bf846c7
jyrkilouhi <jyrkilouhi@gmail.com>
2017-06-06 12:04:00 +0300

    test2

--- src/main/java/tikape/runko/Main.java
+++ src/main/java/tikape/runko/Main.java
@@ -16,6 +16,9 @@ import tikape.runko.domain.Viesti;
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        if (System.getenv("PORT") != null) {
+            port(Integer.valueOf(System.getenv("PORT")));
+        }
         Database database = new Database("jdbc:sqlite:tsjtsfoorumi.db");
         database.init();
 
