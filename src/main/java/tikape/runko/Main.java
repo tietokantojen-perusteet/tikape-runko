ea6981c80c0378e55c0244976c1a9585324d0358
Emma Laitinen <emlaitine@gmail.com>
2017-10-19 12:19:11 +0300

    Hahmottelin Mainia vähän pidemmälle samalla kun pohdin web-puolta.

--- src/main/java/tikape/runko/Main.java
+++ src/main/java/tikape/runko/Main.java
@@ -11,13 +11,43 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Database database = new Database("jdbc:sqlite:smoothietietokanta.db");
         database.createTables();
+        // SmoothieDAO smoothiet = new SmoothieDAO(database);
+        // IngredientDAO raakaaineet = new IngredientDAO(database);
 
         get("/", (req, res) -> {
             HashMap map = new HashMap<>();
-            map.put("viesti", "tervehdys");
+            map.put("smoothiet", "Tähän lista smoothieista");
 
             return new ModelAndView(map, "index");
         }, new ThymeleafTemplateEngine());
+        
+        get("/uusismoothie", (req, res) -> {
+            HashMap map = new HashMap<>();
+            map.put("smoothiet", "Tähän lista smoothieista");
+
+            return new ModelAndView(map, "uusismoothie");
+        }, new ThymeleafTemplateEngine());
+        
+        /*post("/uusismoothie", (req, res) -> {
+            String smoothie = req.queryParams("nimi");
+            smoothies.save(new Smoothie(null, nimi));
+            res.redirect("/uusismoothie");
+            return "";
+        });*/
+        
+        get("/raakaaineet", (req, res) -> {
+            HashMap map = new HashMap<>();
+            map.put("raakaaineet", "Tähän lista raaka-aineista");
+
+            return new ModelAndView(map, "raakaaineet");
+        }, new ThymeleafTemplateEngine());
+        
+        /*post("/raakaaineet", (req, res) -> {
+            String raakaaine = req.queryParams("nimi");
+            raakaaineet.save(new Ingredient(null, nimi));
+            res.redirect("/raakaaineet");
+            return "";
+        });*/
 
         /*get("/opiskelijat", (req, res) -> {
             HashMap map = new HashMap<>();
