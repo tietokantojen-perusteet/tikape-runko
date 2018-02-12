fd7b40f323920b8985d15796d130893e19b8f7c2
Kiilas Kadri <kiilas@vdi-cubbli-021.ad.helsinki.fi>
2018-02-12 19:34:41 +0200

    Käytetään Herokun tarjoamaa porttia.

--- src/main/java/tikape/runko/Main.java
+++ src/main/java/tikape/runko/Main.java
@@ -12,9 +12,9 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
-//        if (System.getenv("PORT") != null) {
-//            Spark.port(Integer.valueOf(System.getenv("PORT")));
-//        }
+        if (System.getenv("PORT") != null) {
+            Spark.port(Integer.valueOf(System.getenv("PORT")));
+        }
         Database database = new Database("jdbc:sqlite:opiskelijat.db");
         database.init();
 
