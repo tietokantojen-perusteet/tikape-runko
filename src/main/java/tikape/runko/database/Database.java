94db8aacf9d9d95d75a26d2a53caf77af0224173
Kiilas Kadri <kiilas@da5-cs-bk107-15.pc.helsinki.fi>
2018-02-12 17:36:25 +0200

    Added a Procfile.

--- src/main/java/tikape/runko/database/Database.java
+++ src/main/java/tikape/runko/database/Database.java
@@ -39,7 +39,7 @@ public class Database {
         ArrayList<String> lista = new ArrayList<>();
 
         // tietokantataulujen luomiseen tarvittavat komennot suoritusjärjestyksessä
-        lista.add("CREATE TABLE Opiskelija (id integer PRIMARY KEY, nimi varchar(255));");
+//        lista.add("CREATE TABLE Opiskelija (id integer PRIMARY KEY, nimi varchar(255));");
         lista.add("INSERT INTO Opiskelija (nimi) VALUES ('Platon');");
         lista.add("INSERT INTO Opiskelija (nimi) VALUES ('Aristoteles');");
         lista.add("INSERT INTO Opiskelija (nimi) VALUES ('Homeros');");
