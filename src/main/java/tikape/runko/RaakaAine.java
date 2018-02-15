6b73ccfd0f344c487833a47b8f5cb1438788b4eb
asdeafas <asdeafas@gmail.com>
2018-02-15 15:57:47 +0200

    Lisäsin mun tiedostot, korjasin Daon pakkauksen

---
+++ src/main/java/tikape/runko/RaakaAine.java
@@ -0,0 +1,33 @@
+
+package tikape.runko;
+
+public class RaakaAine {
+    private Integer id;
+    private String nimi;
+    
+    public RaakaAine(Integer id, String nimi) {
+        this.id=id;
+        this.nimi=nimi;
+    }
+    
+    public RaakaAine(String nimi) {
+        this.id=null;
+        this.nimi=nimi;
+    }
+    
+    public Integer getId() {
+        return this.id;
+    }
+    
+    public void setId(Integer id) {
+        this.id=id;
+    }
+    
+    public String getNimi() {
+        return this.nimi;
+    }
+    
+    public void setNimi(String nimi) {
+        this.nimi=nimi;
+    }
+}
