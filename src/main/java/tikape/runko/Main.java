a8f027293f5cc853cc872ab1f9e553a66132e8ff
Ville Kovanen <villek1995@gmail.com>
2018-02-16 17:16:33 +0200

    Tässä ainakin jonkinlainen pohja Mainille

--- src/main/java/tikape/runko/Main.java
+++ src/main/java/tikape/runko/Main.java
@@ -1,7 +1,9 @@
 package tikape.runko;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import spark.ModelAndView;
+import spark.Spark;
 import static spark.Spark.*;
 import spark.template.thymeleaf.ThymeleafTemplateEngine;
 import tikape.runko.database.Database;
@@ -10,6 +12,23 @@ import tikape.runko.database.Database;
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        ArrayList<Annos> annokset = new ArrayList<>();
+        ArrayList<RaakaAine> raakaAineet = new ArrayList<>();        
+        ArrayList<AnnosRaakaAine> annosRaakaAineet = new ArrayList<>();
         
+        
+        
+        Spark.get("/reseptit", (req, res) -> {
+            HashMap map = new HashMap<>();
+            map.put("annokset", annokset);
+            
+            return new ModelAndView(map, "reseptit");
+        }, new ThymeleafTemplateEngine());
+        
+        Spark.get("/etusivu", (req, res) -> {
+            HashMap map = new HashMap<>();
+
+            return new ModelAndView(map, "etusivu");
+        }, new ThymeleafTemplateEngine());
     }
 }
