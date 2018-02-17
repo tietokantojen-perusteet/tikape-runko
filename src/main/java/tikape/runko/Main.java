30124d7049140998950cc649f8ac60e9835f3446
Ville Kovanen <villek1995@gmail.com>
2018-02-17 19:28:18 +0200

    Hieman edistystä resepteissä

--- src/main/java/tikape/runko/Main.java
+++ src/main/java/tikape/runko/Main.java
@@ -2,25 +2,40 @@ package tikape.runko;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import spark.ModelAndView;
 import spark.Spark;
 import static spark.Spark.*;
 import spark.template.thymeleaf.ThymeleafTemplateEngine;
+import tikape.runko.database.AnnosDao;
 import tikape.runko.database.Database;
 
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        ArrayList<Annos> annokset = new ArrayList<>();
-        ArrayList<RaakaAine> raakaAineet = new ArrayList<>();        
-        ArrayList<AnnosRaakaAine> annosRaakaAineet = new ArrayList<>();
+        Database database = new Database("osoite");
         
         
+        
+        
+        AnnosDao annosDao = new AnnosDao(database);
         
         Spark.get("/reseptit", (req, res) -> {
+            List<Annos> annokset = new ArrayList<>();    
+            List<AnnosRaakaAine> annosRaakaAineet = new ArrayList<>();
+            
+            annokset = annosDao.findAll();
+            
+            //Selvitetään seuraavaksi annoksiin sisältyvät raaka-aineet
+            //Tallennetaan nämä HashMappiin, jossa avaimena on annos ja avaimeen liittyvänä
+            //arvona lista, jossa on annokset raaka-aineet
+            
+            HashMap<Annos, List<RaakaAine>> raakaAineet = new HashMap<>();
+            
             HashMap map = new HashMap<>();
             map.put("annokset", annokset);
+            map.put("raakaAineet", raakaAineet);
             
             return new ModelAndView(map, "reseptit");
         }, new ThymeleafTemplateEngine());
