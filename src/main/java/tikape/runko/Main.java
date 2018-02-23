101845ea934dd711c2a4756ebc9b50846157ca7b
Kiilas Kadri <kiilas@da5-cs-b221-24.pc.helsinki.fi>
2018-02-23 15:22:57 +0200

    bla

--- src/main/java/tikape/runko/Main.java
+++ src/main/java/tikape/runko/Main.java
@@ -15,6 +15,9 @@ public class Main {
         if (System.getenv("PORT") != null) {
             Spark.port(Integer.valueOf(System.getenv("PORT")));
         }
+        
+        
+       
         Database database = new Database("jdbc:sqlite:opiskelijat.db");
         database.init();
 
