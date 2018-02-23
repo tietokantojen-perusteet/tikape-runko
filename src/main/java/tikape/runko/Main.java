118e52cd94ec84d2f53ac566b35707f109167a79
Kiilas Kadri <kiilas@da5-cs-b221-24.pc.helsinki.fi>
2018-02-23 16:01:06 +0200

    kokeilen, toimiiko branch

--- src/main/java/tikape/runko/Main.java
+++ src/main/java/tikape/runko/Main.java
@@ -1,5 +1,7 @@
 package tikape.runko;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.util.HashMap;
 import spark.ModelAndView;
 import spark.Spark;
@@ -15,14 +17,12 @@ public class Main {
         if (System.getenv("PORT") != null) {
             Spark.port(Integer.valueOf(System.getenv("PORT")));
         }
-        
-        
-       
-        Database database = new Database("jdbc:sqlite:opiskelijat.db");
+
+        Database database = new Database("jdbc:sqlite:reseptit.db");
         database.init();
 
         OpiskelijaDao opiskelijaDao = new OpiskelijaDao(database);
-
+        System.out.println("bla");
         get("/", (req, res) -> {
             HashMap map = new HashMap<>();
             map.put("viesti", "tervehdys");
@@ -43,5 +43,14 @@ public class Main {
 
             return new ModelAndView(map, "opiskelija");
         }, new ThymeleafTemplateEngine());
+    }
+
+    public static Connection getConnection() throws Exception {
+        String dbUrl = System.getenv("JDBC_DATABASE_URL");
+        if (dbUrl != null && dbUrl.length() > 0) {
+            return DriverManager.getConnection(dbUrl);
+        }
+
+        return DriverManager.getConnection("jdbc:sqlite:reseptit.db");
     }
 }
